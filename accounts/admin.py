09df21ac57247278d427dfc67cd3da3313953934
Miguel <miguel_guerra23242@elpoli.edu.co>
2025-10-16 16:10:02 -0500

    Actualización con lo que pedia el taller

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Project
+from products.models import Product
 
-@admin.register(Project)
-class ProjectAdmin(admin.ModelAdmin):
-    list_display = ("id", "name")   
-    search_fields = ("name",)
+admin.site.site_header = "IvorySpa-Shop Administración"
+admin.site.site_title = "IvorySpa-Shop Portal Admin"
+admin.site.index_title = "Bienvenido al Panel de Administración de IvorySpa-Shop"
---
+++ ivoryspa_shop/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+admin.site.site_header = "IvorySpa-Shop Administración"
+admin.site.site_title = "IvorySpa-Shop Portal Admin"
+admin.site.index_title = "Bienvenido a la Administración de IvorySpa-Shop"
