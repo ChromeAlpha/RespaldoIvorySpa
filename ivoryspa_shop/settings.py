1023788532f028dbb1a0a9ecb6a941b569f38364
Miguel <miguel_guerra23242@elpoli.edu.co>
2025-10-27 15:58:34 -0500

    Arreglando mas cosas

--- ivoryspa_shop/settings.py
+++ ivoryspa_shop/settings.py
@@ -51,7 +51,6 @@ LOGIN_REDIRECT_URL = '/home/'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -136,6 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'staticfiles/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
