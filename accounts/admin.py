bb599f2ca0b1e32dfdcfc4a14d82a1e6587dc91e
Miguel <miguel_guerra23242@elpoli.edu.co>
2025-10-27 12:50:54 -0500

    Personalización del admin y ajustes finales

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Project
-from products.models import Product
 
-admin.site.site_header = "IvorySpa-Shop Administración"
-admin.site.site_title = "IvorySpa-Shop Portal Admin"
-admin.site.index_title = "Bienvenido al Panel de Administración de IvorySpa-Shop"
+admin.site.site_header = "IvorySpa-Shop Admin"
+admin.site.site_title = "IvorySpa-Shop Panel"
+admin.site.index_title = "Bienvenido al Panel de Administración"
+
